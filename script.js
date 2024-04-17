e22b0a2f718c3fa4a4c5b4a95ba8c5a93d4be42a
Seki356 <167305662+Seki356@users.noreply.github.com>
2024-04-17 21:48:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -20,35 +20,56 @@ $(document).ready(function() {
     var peopleCount = 0;
     var eventCount = 0;
 
-    function toggleWord($ele, data, isPerson) {
-        var randomYear;
-        var isName = $ele.data('isName');
-    
-        if (isName) {
-            randomYear = isPerson ? Math.floor(Math.random() * (data.endYear - data.startYear + 1)) + data.startYear : data.year;
-            setTimeout(function() { // 添加延迟处理
-                $ele.text(randomYear).data({ isName: false, randomYear: randomYear }).addClass('yearElement');
-                isElementActive = true;
-                adjustElementsOpacityAndSpeed(randomYear); // 更新其他元素透明度和速度
-            }, 300); // 设置300毫秒的延迟
-        } else {
-            $ele.text(data.name).data({ isName: true, randomYear: null }).removeClass('yearElement');
-            isElementActive = false;
-            adjustElementsOpacityAndSpeed(); // 重置透明度和速度
+    function createFloatingElement(data, isPerson) {
+        if (elements.length >= maxElements) {
+            return;
         }
-    
-        $('body').toggleClass('noYearActive', !isElementActive);
-    }
-    
-    function adjustElementsOpacityAndSpeed(randomYear) {
-        elements.forEach(function($otherEle, index) {
-            if ($otherEle.data('isName')) {
-                adjustOpacityAndSpeed($otherEle, $otherEle.data(), randomYear, index);
+
+        var $element = $('<div>', {
+            text: data.name,
+            class: 'floatingText',
+        }).appendTo('body').on('click touchend', function(event) {  // Changed touchstart to touchend
+            if (!isElementActive || $(this).data('isName') === false) {
+                toggleWord($(this), data, isPerson);
+            } else if ($(this).data('isName')) {
+                // Avoiding preventDefault to not block navigation
+                window.open(data.website, '_blank');
             }
         });
+
+        var elementWidth = $element.outerWidth();
+        var elementHeight = $element.outerHeight();
+
+        var pos = {
+            x: ($(window).width() - elementWidth) / 2,
+            y: ($(window).height() - elementHeight) / 2
+        };
+
+        $element.css({
+            left: pos.x + 'px',
+            top: pos.y + 'px',
+        });
+
+        $element.data(isPerson ? {
+            isName: true,
+            startYear: data.startYear,
+            endYear: data.endYear,
+            website: data.website  // Ensure website link is included
+        } : {
+            isName: true,
+            year: data.year,
+            website: data.website  // Ensure website link is included
+        });
+
+        var speedModifier = $(window).width() > $(window).height() ? { x: 1.5, y: 1 } : { x: 1, y: 1.5 };
+        velocities.push({
+            x: (Math.random() - 0.5) * normalSpeed * speedModifier.x,
+            y: (Math.random() - 0.5) * normalSpeed * speedModifier.y
+        });
+
+        elements.push($element);
     }
 
-    
     function toggleWord($ele, data, isPerson) {
         var randomYear;
         $ele.css('opacity', '1');
