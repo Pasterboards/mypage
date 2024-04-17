0cb417488973652589be1bf80e2fa0058e30aa26
Seki356 <167305662+Seki356@users.noreply.github.com>
2024-04-17 20:59:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -28,15 +28,29 @@ $(document).ready(function() {
         var $element = $('<div>', {
             text: data.name,
             class: 'floatingText',
-        }).appendTo('body').on('click touchstart', function(event) {
+        }).appendTo('body');
+    
+        // 使用一个函数来处理点击或触摸事件
+        function handleInteraction(event) {
             event.preventDefault();
-            if (!isElementActive || $(this).data('isName') === false) {
-                toggleWord($(this), data, isPerson);
-            } else if ($(this).data('isName') === true) {
-                // 当元素处于显示名称状态时，点击打开新窗口至指定网站
-                window.open($(this).data('website'), '_blank');
+            if (!isElementActive || $element.data('isName') === false) {
+                toggleWord($element, data, isPerson);
+            } else if ($element.data('isName') === true) {
+                // 检查是否是移动设备，如果是，则导航至外部链接，否则打开新窗口
+                if ('ontouchstart' in window) {
+                    window.location.href = $element.data('website');
+                } else {
+                    window.open($element.data('website'), '_blank');
+                }
             }
-        });
+        }
+    
+        // 检测是否是触摸设备
+        if ('ontouchstart' in window) {
+            $element.on('touchstart', handleInteraction);
+        } else {
+            $element.on('click', handleInteraction);
+        }
     
         var elementWidth = $element.outerWidth();
         var elementHeight = $element.outerHeight();
@@ -55,11 +69,11 @@ $(document).ready(function() {
             isName: true,
             startYear: data.startYear,
             endYear: data.endYear,
-            website: data.website // 确保网站链接被加入到元素的数据中
+            website: data.website
         } : {
             isName: true,
             year: data.year,
-            website: data.website // 同上
+            website: data.website
         });
     
         var speedModifier = $(window).width() > $(window).height() ? { x: 1.5, y: 1 } : { x: 1, y: 1.5 };
