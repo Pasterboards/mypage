2614686146fbfb0d49f7c1783a7a23f0d9e77efb
Seki356 <167305662+Seki356@users.noreply.github.com>
2024-04-17 23:23:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -21,28 +21,32 @@ $(document).ready(function() {
     var eventCount = 0;
 
     $(document).ready(function() {
-        // 添加的变量用于记录触摸开始时间和设置触摸时长的阈值
-        var touchStartTime = 0;  // 存储触摸开始的时间
-        var minimumTouchDuration = 10;  // 设置触摸时长的阈值，单位为毫秒
+        // 存储触摸开始时间和设置触摸时长的阈值
+        var touchStartTime = 0;
+        var minimumTouchDuration = 500;  // 触摸时长的阈值，单位为毫秒
     
-        // 添加的触摸开始事件处理器
+        // 处理触摸开始事件
         $('body').on('touchstart', '.floatingText', function(event) {
-            touchStartTime = Date.now();  // 记录触摸开始的时间
+            touchStartTime = Date.now();  // 记录触摸开始时间
         });
     
-        // 修改的触摸结束事件处理器，加入了时间判断
+        // 处理触摸结束事件，同时适用于数字和文本的切换
         $('body').on('touchend', '.floatingText', function(event) {
-            var touchEndTime = Date.now();  // 记录触摸结束的时间
-            var touchDuration = touchEndTime - touchStartTime;  // 计算触摸持续的时间
-            if (touchDuration > minimumTouchDuration) {  // 如果触摸时间超过阈值，则执行原有的点击逻辑
-                if (!isElementActive || $(this).data('isName') === false) {
-                    toggleWord($(this), data, isPerson);
-                } else if ($(this).data('isName')) {
-                    window.open(data.website, '_blank');
+            var touchEndTime = Date.now();
+            var touchDuration = touchEndTime - touchStartTime;
+            if (touchDuration > minimumTouchDuration) {  // 检查触摸时长是否超过阈值
+                var isName = $(this).data('isName');
+                if (!isElementActive || !isName) {
+                    toggleWord($(this), $(this).data(), $(this).data('isPerson'));
+                } else if (isName) {
+                    window.open($(this).data('website'), '_blank');
                 }
             }
         });
     });
+    
+    // toggleWord 函数和其它部分保持不变
+    
     
     function createFloatingElement(data, isPerson) {
         if (elements.length >= maxElements) {
