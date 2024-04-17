76506d846bee77282c7c96d32832c0577f544dd4
Pasterboard <pasterboard@PasterboarddeMacBook-Pro.local>
2024-04-17 19:15:43 +0900

    init

---
+++ data2024.js
@@ -0,0 +1,27 @@
+var peopleData = [
+    {name: 'Sannomiya Lab.', startYear: 2017, endYear: 2019, website: 'https://www.sannomiya.iem.titech.ac.jp/indexEN.html'}, 
+    { name: "Structural Materials Designing Laboratory", startYear: 2016, endYear: 2017 , website: 'https://hirosawalab.ynu.ac.jp/oldsite/index.html'},
+    { name: "Diskunion", startYear: 2018, endYear: 2020 , website: 'https://diskunion.net/shop/ct/unionrecord_shinjuku'},
+    { name: "Thirteen Road", startYear: 2012, endYear: 2015, website: 'https://site.douban.com/239267/' },
+    { name: "Muxu", startYear: 2016, endYear: 2024, website: 'https://cloversounds.bandcamp.com/album/cycle' },
+    { name: "Piping Engineer Job", startYear: 2020, endYear: 2022, website: 'https://www.sumitomo-chem.co.jp/news/detail/20220411.html' },
+    { name: "Zuper Cold", startYear: 2020, endYear: 2021, website: 'https://mp.weixin.qq.com/s/35b80_k8PQZ_oRNgqU-4WA' },
+    { name: "COVID-19", startYear: 2019, endYear: 2023, website: 'https://en.wikipedia.org/wiki/COVID-19' },
+    { name: "Baystars Fan", startYear: 2017, endYear: 2024, website: 'https://www.baystars.co.jp/'},
+    { name: "Discog", startYear: 2018, endYear: 2024, website: 'https://www.discogs.com/ja/user/Pasteboard_/collection'},
+    { name: "Instagram", startYear: 2015, endYear: 2024, website: 'https://www.instagram.com/pasterboard__/'},
+    { name: "Polaroid Her", startYear: 2020, endYear: 2021, website: 'https://www.tumblr.com/blog_auth/postceremony'},
+   
+];
+
+var eventData = [
+    { name: "1994", year: 1994, website: 'https://en.wikipedia.org/wiki/1994' },
+    { name: "We Stand So Many Ghosts of Unknown Chances", year: 2024, website: 'https://gallery-alpham.com/exhibition/project_2023-2024/vol5/' },
+    { name: "Pasteboard's Vinyl Club", year: 2023 , website: 'https://geisai.jp/tenji_search/map/outside/T075'},
+    { name: "Fabrication and Optical Measurement of Hexagonal Nanodisk Arrays", year: 2019, website: 'https://www.jstage.jst.go.jp/article/jsapmeeting/2019.1/0/2019.1_1053/_article/-char/en'},
+    { name: "Tokyo Biennale", year: 2023, website: 'https://immigration-museum-tokyo.com/2023-archive/wts/'},
+    { name: "Concept Designing", year: 2018, website: 'https://partner-web.jp/article/?id=1705'},
+    { name: "Damien Rice", year: 2017, website: 'https://youtu.be/pk6JoI65Zs0?si=nvBIK0aLPJuX-TJ2'},
+    { name: "Hamacho Zouenkeikaku", year: 2024, website: 'https://taikafasciation.bandcamp.com/album/hamacho-zouenkeikaku'},
+];
+
---
+++ script.js
@@ -0,0 +1,174 @@
+$(document).ready(function() {
+    console.log(peopleData);
+    console.log(eventData);
+
+    var elements = [];
+    var velocities = [];
+    var isAnimating = true;
+    var isElementActive = false;
+    var normalSpeed = 0.4;
+    var slowedSpeed = 0.1;
+
+    var isMobileBrowser = $(window).width() < 768;
+    if (isMobileBrowser) {
+        normalSpeed *= 2;
+        slowedSpeed *= 2;
+    }
+
+    var maxElements = isMobileBrowser ? 35 : Infinity;
+
+    var peopleCount = 0;
+    var eventCount = 0;
+
+    function createFloatingElement(data, isPerson) {
+        if (elements.length >= maxElements) {
+            return;
+        }
+
+        var $element = $('<div>', {
+            text: data.name,
+            class: 'floatingText',
+        }).appendTo('body').on('click touchend', function(event) {  // Changed touchstart to touchend
+            if (!isElementActive || $(this).data('isName') === false) {
+                toggleWord($(this), data, isPerson);
+            } else if ($(this).data('isName')) {
+                // Avoiding preventDefault to not block navigation
+                window.open(data.website, '_blank');
+            }
+        });
+
+        var elementWidth = $element.outerWidth();
+        var elementHeight = $element.outerHeight();
+
+        var pos = {
+            x: ($(window).width() - elementWidth) / 2,
+            y: ($(window).height() - elementHeight) / 2
+        };
+
+        $element.css({
+            left: pos.x + 'px',
+            top: pos.y + 'px',
+        });
+
+        $element.data(isPerson ? {
+            isName: true,
+            startYear: data.startYear,
+            endYear: data.endYear,
+            website: data.website  // Ensure website link is included
+        } : {
+            isName: true,
+            year: data.year,
+            website: data.website  // Ensure website link is included
+        });
+
+        var speedModifier = $(window).width() > $(window).height() ? { x: 1.5, y: 1 } : { x: 1, y: 1.5 };
+        velocities.push({
+            x: (Math.random() - 0.5) * normalSpeed * speedModifier.x,
+            y: (Math.random() - 0.5) * normalSpeed * speedModifier.y
+        });
+
+        elements.push($element);
+    }
+
+    function toggleWord($ele, data, isPerson) {
+        var randomYear;
+        $ele.css('opacity', '1');
+        var isName = $ele.data('isName');
+
+        if (isName) {
+            randomYear = isPerson ? Math.floor(Math.random() * (data.endYear - data.startYear + 1)) + data.startYear : data.year;
+            $ele.text(randomYear).data({ isName: false, randomYear: randomYear }).addClass('yearElement');
+            isElementActive = true;
+        } else {
+            $ele.text(data.name).data({ isName: true, randomYear: null }).removeClass('yearElement');
+            isElementActive = false;
+        }
+
+        elements.forEach(function($otherEle, index) {
+            if ($otherEle.data('isName')) {
+                adjustOpacityAndSpeed($otherEle, $otherEle.data(), randomYear, index);
+            }
+        });
+
+        isAnimating = false;
+        $('body').toggleClass('noYearActive', !isElementActive);
+    }
+
+    function adjustOpacityAndSpeed($ele, data, randomYear, index) {
+        if ((data.startYear <= randomYear && data.endYear >= randomYear) || (data.year === randomYear)) {
+            $ele.css('opacity', '1');
+            velocities[index] = { x: (Math.random() - 0.5) * slowedSpeed, y: (Math.random() - 0.5) * slowedSpeed };
+        } else {
+            $ele.css('opacity', '0.3');
+            velocities[index] = { x: (Math.random() - 0.5) * normalSpeed, y: (Math.random() - 0.5) * normalSpeed };
+        }
+    }
+
+    peopleData.forEach(function(data) {
+        if (isMobileBrowser && peopleCount < maxElements / 2) {
+            createFloatingElement(data, true);
+            peopleCount++;
+        } else if (!isMobileBrowser) {
+            createFloatingElement(data, true);
+        }
+    });
+
+    eventData.forEach(function(data) {
+        if (isMobileBrowser && eventCount < maxElements / 2) {
+            createFloatingElement(data, false);
+            eventCount++;
+        } else if (!isMobileBrowser) {
+            createFloatingElement(data, false);
+        }
+    });
+
+    $('body').addClass('noYearActive');
+
+    $(document).on('click touchend', function(event) {  // Changed touchstart to touchend globally
+        if (!$(event.target).closest('.floatingText').length) {
+            isAnimating = true;
+        }
+    });
+
+    function animateElements() {
+        if (isAnimating) {
+            elements.forEach(function($ele, index) {
+                var velocity = velocities[index];
+                var newPos = calculateNewPosition($ele, velocity);
+                $ele.css({ left: newPos.x, top: newPos.y });
+
+                if ($ele.data('isName') === false) {
+                    velocities[index] = {
+                        x: (Math.random() - 0.5) * slowedSpeed,
+                        y: (Math.random() - 0.5) * slowedSpeed
+                    };
+                }
+            });
+        }
+        requestAnimationFrame(animateElements);
+    }
+
+    function calculateNewPosition($ele, velocity) {
+        var newPos = {
+            x: parseFloat($ele.css('left')) + velocity.x,
+            y: parseFloat($ele.css('top')) + velocity.y
+        };
+        var eleWidth = $ele.outerWidth();
+        var eleHeight = $ele.outerHeight();
+        var windowWidth = $(window).width();
+        var windowHeight = $(window).height();
+
+        if (newPos.x < 0 || newPos.x + eleWidth > windowWidth) {
+            velocity.x = -velocity.x;
+            newPos.x = Math.max(0, Math.min(newPos.x, windowWidth - eleWidth));
+        }
+        if (newPos.y < 0 || newPos.y + eleHeight > windowHeight) {
+            velocity.y = -velocity.y;
+            newPos.y = Math.max(0, Math.min(newPos.y, windowHeight - eleHeight));
+        }
+
+        return newPos;
+    }
+
+    requestAnimationFrame(animateElements);
+});
